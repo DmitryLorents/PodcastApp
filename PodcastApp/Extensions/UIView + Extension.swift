5fea1386bc64230b2b1dd49b701fd451988a2d19
DmitryLorents <zmey.task@gmail.com>
2023-11-04 09:33:58 +0300

    Add UIView extension

---
+++ PodcastApp/Extensions/UIView + Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIView + Extension.swift
+//  PodcastApp
+//
+//  Created by Dmitry Lorents on 04.11.2023.
+//
+
+import UIKit
+
+extension UIView {
+    func addViews(views: UIView...) {
+        for view in views {
+            self.addSubview(view)
+        }
+    }
+}
